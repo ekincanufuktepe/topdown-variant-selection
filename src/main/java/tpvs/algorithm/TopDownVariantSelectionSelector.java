646af2cd018239cb8f75bf49f534e7367da60e75
ekincanufuktepe <ekincanufuktepe@gmail.com>
2022-06-02 14:05:36 -0500

    Defining the data type and the data structure
    This class just inherits a Boolean type ArrayList, which defines our
    problem's data type and structure.

---
+++ src/main/java/tpvs/algorithm/TopDownVariantSelectionSelector.java
@@ -0,0 +1,8 @@
+package tpvs.algorithm;
+
+import java.util.ArrayList;
+
+public class TopDownVariantSelectionSelector extends ArrayList<Boolean>{
+
+	private static final long serialVersionUID = 1L;
+}
