a777bfc91b308c89244a20ab31ea238bf673ee33
ekincanufuktepe <ekincanufuktepe@gmail.com>
2022-06-02 14:09:24 -0500

    Decoder class for converting genotype to phenotype

---
+++ src/main/java/tpvs/algorithm/TopDownVariantSelectionDecoder.java
@@ -0,0 +1,15 @@
+package tpvs.algorithm;
+
+import org.opt4j.core.genotype.BooleanGenotype;
+import org.opt4j.core.problem.Decoder;
+
+public class TopDownVariantSelectionDecoder implements Decoder<BooleanGenotype, TopDownVariantSelectionSelector> {
+
+	public TopDownVariantSelectionSelector decode(BooleanGenotype genotype) {
+		TopDownVariantSelectionSelector selection = new TopDownVariantSelectionSelector();
+		for(Boolean i : genotype) {
+			selection.add(i);
+		}
+		return selection;
+	}
+}
