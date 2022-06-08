dcdd9d16b076177f4e86e5cdb9bdfc0998c1a774
ekincanufuktepe <ekincanufuktepe@gmail.com>
2022-06-07 19:44:20 -0500

    Default constructor added for Decoder
    To create an instance of the Decoder class a default constructor is
    added. The decode method will be used to decode the genotype to a
    phenotype later.

--- src/main/java/tpvs/algorithm/TopDownVariantSelectionDecoder.java
+++ src/main/java/tpvs/algorithm/TopDownVariantSelectionDecoder.java
@@ -5,6 +5,9 @@ import org.opt4j.core.problem.Decoder;
 
 public class TopDownVariantSelectionDecoder implements Decoder<BooleanGenotype, TopDownVariantSelectionSelector> {
 
+	public TopDownVariantSelectionDecoder() {
+	}
+	
 	public TopDownVariantSelectionSelector decode(BooleanGenotype genotype) {
 		TopDownVariantSelectionSelector selection = new TopDownVariantSelectionSelector();
 		for(Boolean i : genotype) {
